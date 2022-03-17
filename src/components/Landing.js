f169fff164f88ffcab8e6b699ba5e15644e3e06e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-17 17:13:49 +0300

    Составил структуру проекта

---
+++ src/components/Landing.js
@@ -0,0 +1,26 @@
+import React from "react";
+import restaurants from '../sample-restaurants'
+
+class Landing extends React.Component {
+  render() {
+     
+    return (
+      <div className="restaurant_select">
+        <div className="restaurant_select_top">
+          <div className="restaurant_select_top_header">Выбери ресторан</div>
+          <div className="arrow_picker-up"></div>
+          <div className="arrow_pecker-down"></div>
+        </div>
+        <div className="restaurant_select_bottom">
+          <ul>
+           { restaurants.map(restaurant => {
+          return <li key={restaurant.id}>{restaurant.title}</li>
+      })}
+          </ul>
+        </div>
+        <button>Перейти в ресторан</button>
+      </div>
+    );
+  }
+}
+export default Landing;
