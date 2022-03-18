94ca62d016cdafcaed258c6312f9b81658530b85
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-18 12:27:01 +0300

    Настройка маршрутов

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,17 @@ import Header from "./Header";
 import MenuAdmin from "./MenuAdmin";
 import Order from "./Order";
 
-
 class App extends React.Component {
-    render () {
-        return (
-            <div className="burger-paradise">
-                <div className="menu">
-                    <Header title='Very Hot Burger'/>
-                   
-                </div>
-                <Order/>
-               <MenuAdmin/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="burger-paradise">
+        <div className="menu">
+          <Header title="Very Hot Burger" />
+        </div>
+        <Order />
+        <MenuAdmin />
+      </div>
+    );
+  }
 }
 export default App;
