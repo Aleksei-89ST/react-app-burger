3fffd8d5c9d48112c7909d8b4d53e2bc55daed31
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-18 11:33:42 +0300

    create component MenuAdmin

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import MenuAdmin from "./MenuAdmin";
 import Order from "./Order";
 
 
@@ -11,7 +12,7 @@ class App extends React.Component {
                     <Header/>
                 </div>
                 <Order/>
-                {/* <MenuAdmin/> */}
+               <MenuAdmin/>
             </div>
         )
     }
