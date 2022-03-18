8a410535d06f0eee67ad06d54b2594f587296e8b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-18 11:26:27 +0300

    create component Header

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+
+class App extends React.Component {
+    render () {
+        return (
+            <div className="burger-paradise">
+                <div className="menu">
+                    <Header/>
+                </div>
+                {/* <Order/>
+                <MenuAdmin/> */}
+            </div>
+        )
+    }
+}
+export default App;
