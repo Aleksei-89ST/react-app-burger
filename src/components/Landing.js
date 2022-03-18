be2460a7be5c1348f97bc65d09e719045e59c3cd
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-18 10:29:18 +0300

    добавил стили css

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ class Landing extends React.Component {
     return (
       <div className="restaurant_select">
         <div className="restaurant_select_top">
-          <div className="restaurant_select_top_header">Выбери ресторан</div>
+          <div className="restaurant_select_top_header font-effect-outline">Выбери ресторан</div>
           <div className="arrow_picker-up"></div>
           <div className="arrow_pecker-down"></div>
         </div>
