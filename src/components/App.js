c3f689202dfe9fd367308814f1e5281870c99cbc
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-21 11:41:14 +0300

    теперь можно добавлять бургер на заказ

--- src/components/App.js
+++ src/components/App.js
@@ -2,38 +2,56 @@ import React from "react";
 import Header from "./Header";
 import MenuAdmin from "./MenuAdmin";
 import Order from "./Order";
-import sampleBurgers from '../sample-burgers'
+import sampleBurgers from "../sample-burgers";
 import Burger from "./Burger";
 
 class App extends React.Component {
   state = {
-    burgers:{},
-    order:{}
-  }
-  addBurger = burger => {
-        //1.Делаем копию объекта state
-        const burgers = {...this.state.burgers};
-        //2.Добавить новый бургер в переменную burgers
-        burgers[`burger${Date.now()}`]=burger;
-        //3.Записать наш новый обьект бургерс в state
-        this.setState({burgers})
-  }
+    burgers: {},
+    order: {},
+  };
+  addBurger = (burger) => {
+    //1.Делаем копию объекта state
+    const burgers = { ...this.state.burgers };
+    //2.Добавить новый бургер в переменную burgers
+    burgers[`burger${Date.now()}`] = burger;
+    //3.Записать наш новый обьект бургерс в state
+    this.setState({ burgers });
+  };
   loadSampleBurgers = () => {
-    this.setState({burgers:sampleBurgers})
-  }
+    this.setState({ burgers: sampleBurgers });
+  };
+  addToOrder = (key) => {
+    //1.Делаем копию объекта state
+    const order = { ...this.state.order };
+    //2.Добавить ключ к заказу со значением 1 либо обновить текущее значение
+    order[key] = order[key] + 1 || 1;
+    //3.Записать наш новый обьект order в state
+    this.setState({ order });
+  };
   render() {
     return (
       <div className="burger-paradise">
         <div className="menu">
           <Header title="Very Hot Burger" />
           <ul className="burgers">
-{Object.keys(this.state.burgers).map(key => {
-  return <Burger key={key} index={key} details={this.state.burgers[key]}/>
-})}
+            {Object.keys(this.state.burgers).map((key) => {
+              return (
+                <Burger
+                  key={key}
+                  index={key}
+                  addToOrder={this.addToOrder}
+                  details={this.state.burgers[key]}
+                />
+              );
+            })}
           </ul>
         </div>
         <Order />
-        <MenuAdmin addBurger={this.addBurger} loadSampleBurgers={this.loadSampleBurgers}/>
+        <MenuAdmin
+          addBurger={this.addBurger}
+          loadSampleBurgers={this.loadSampleBurgers}
+        />
       </div>
     );
   }
--- src/components/Burger.js
+++ src/components/Burger.js
@@ -2,18 +2,29 @@ import React from "react";
 
 class Burger extends React.Component {
   render() {
-      const {image,name,price,desc,status} = this.props.details;
-    return <li className="menu-burger">
+    const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
+    return (
+      <li className="menu-burger">
         <div className="image-container">
-            <img src={image} alt={name}/>
+          <img src={image} alt={name} />
         </div>
         <div className="burger-details">
-            <h3 className="burger-name">{name}
-             <span className="price">{price} ₽ </span> </h3>
-             <p>{desc}</p>
-             <button className="buttonOrder">Заказать</button>
+          <h3 className="burger-name">
+            {name}
+            <span className="price">{price} ₽ </span>{" "}
+          </h3>
+          <p>{desc}</p>
+          <button
+            className="buttonOrder"
+            disabled={!isAvailable}
+            onClick={() => this.props.addToOrder(this.props.index)}
+          >
+            {isAvailable ? "Заказать" : "Временно нет"}
+          </button>
         </div>
-    </li>
+      </li>
+    );
   }
 }
 export default Burger;
