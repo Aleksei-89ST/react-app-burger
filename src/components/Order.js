189efbff89adc412e9405c2d35f8087e56933e42
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-21 13:46:08 +0300

    дописал логику - (Ваш заказ)

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,26 +1,52 @@
 import React from "react";
+import Shipment from "./Shipment";
 
 class Order extends React.Component {
+  renderOrder = (key) => {
+    const burger = this.props.burgers[key];
+    const count = this.props.order[key];
+    const isAvailable = burger && burger.status === "available";
+    if (!isAvailable) {
+      return (
+        <li className="unavailible" key={key}>
+          Извините,{burger ? burger.name : "burger"} временно недоступен
+        </li>
+      );
+    }
+
+    return (
+      <li key={key}>
+        <span>
+          <span>{count}</span>
+          шт. {burger.name}
+          <span> {count * burger.price} ₽ </span>
+          <button className="cansellItem">&times;</button>
+        </span>
+      </li>
+    );
+  };
   render() {
     const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce((prevTotal,key)=> {
-        const burger = this.props.burgers[key];
-        const count = this.props.order[key];
-        const isAvailable = burger && burger.status === 'available';
-        if (isAvailable) {
-            return prevTotal + burger.price * count;
-        }
-       return prevTotal ;
-    },0)
+    const total = orderIds.reduce((prevTotal, key) => {
+      const burger = this.props.burgers[key];
+      const count = this.props.order[key];
+      const isAvailable = burger && burger.status === "available";
+      if (isAvailable) {
+        return prevTotal + burger.price * count;
+      }
+      return prevTotal;
+    }, 0);
     return (
       <div className="order-wrap">
         <h2>Ваш Заказ</h2>
-        <ul className="order">
-          {orderIds.map((key) => {
-            return <li>{key}</li>;
-          })}
-        </ul>
-        {total}
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        {total > 0 ? (
+          <Shipment total={total} />
+        ) : (
+          <div className="nothingSelected">
+            Выберите блюда и добавьте к заказу
+          </div>
+        )}
       </div>
     );
   }
