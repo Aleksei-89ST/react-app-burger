746228d839be9cdf070875513f6308d08d6e6dc2
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-21 10:59:17 +0300

    практически закончил с компонентом бургер

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -2,9 +2,16 @@ import React from "react";
 
 class Burger extends React.Component {
   render() {
+      const {image,name,price,desc,status} = this.props.details;
     return <li className="menu-burger">
         <div className="image-container">
-            <img src={this.props.details.image}/>
+            <img src={image} alt={name}/>
+        </div>
+        <div className="burger-details">
+            <h3 className="burger-name">{name}
+             <span className="price">{price} ₽ </span> </h3>
+             <p>{desc}</p>
+             <button className="buttonOrder">Заказать</button>
         </div>
     </li>
   }
