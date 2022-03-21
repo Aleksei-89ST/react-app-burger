dbab901080310b7950f5192e6fd88427a7a27897
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-21 10:47:05 +0300

    теперь загружаются изображения бургеров

---
+++ src/components/Burger.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Burger extends React.Component {
+  render() {
+    return <li className="menu-burger">
+        <div className="image-container">
+            <img src={this.props.details.image}/>
+        </div>
+    </li>
+  }
+}
+export default Burger;
