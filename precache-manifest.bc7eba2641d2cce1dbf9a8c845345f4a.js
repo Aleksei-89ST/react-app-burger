a88a6879e6827121d2fb517aa433604216f2a990
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-24 11:32:30 +0300

    Updates

---
+++ precache-manifest.bc7eba2641d2cce1dbf9a8c845345f4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48db8260cab6e4a8afac36ae5bfd963",
+    "url": "/react-app-burger/index.html"
+  },
+  {
+    "revision": "8d7cf4a90d4994fad52f",
+    "url": "/react-app-burger/static/css/main.57903140.chunk.css"
+  },
+  {
+    "revision": "99b08e5aef6d8ed9e653",
+    "url": "/react-app-burger/static/js/2.df3b206a.chunk.js"
+  },
+  {
+    "revision": "8d7cf4a90d4994fad52f",
+    "url": "/react-app-burger/static/js/main.843af1ab.chunk.js"
+  },
+  {
+    "revision": "b4d36170e29ce2e88468",
+    "url": "/react-app-burger/static/js/runtime-main.62c3c03e.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/react-app-burger/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/react-app-burger/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/react-app-burger/static/media/up.551b5c6b.svg"
+  }
+]);
